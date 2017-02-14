bff248413f6657bad9eb1a88f69d340677837f05
Cristina <mcmguaba@gmail.com>
2017-02-14 09:40:12 -0500

    Update Counter.jl

--- src/Counter.jl
+++ src/Counter.jl
@@ -2,12 +2,13 @@ __precompile__(true)
 
 module Counter
 
-function count_non_fives(start::Integer, finish::Integer)  # type declarations optional
-    a = start:finish  # define a range
-    counter = 0
-    for i = a
-        if !contains(string(i), "5")
-            counter += 1
+    function count_non_fives(start::Integer, finish::Integer)  # type declarations optional
+        a = start:finish  # define a range
+        counter = 0
+        for i = a
+            if !contains(string(i), "5")
+                counter += 1
+            end
         end
     end
 end
